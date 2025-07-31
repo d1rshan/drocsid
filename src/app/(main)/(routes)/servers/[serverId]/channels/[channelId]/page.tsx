9377cd6e3570ecf72f93dfc91e19db680d9cc1b5
darshan <darshan.paccha@gmail.com>
2025-07-31 11:56:11 +0530

    add channel id age, member conversations page

---
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,44 @@
+import { redirect } from "next/navigation";
+
+import db from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { ChatHeader } from "@/components/chat/chat-header/page";
+
+interface ChannelIdPageProps {
+  params: Promise<{
+    serverId: string;
+    channelId: string;
+  }>;
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
+  const { serverId, channelId } = await params;
+
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name} serverId={serverId} type="channel" />
+    </div>
+  );
+}
--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,3 +1,46 @@
-export default function ServerIdPage() {
-  return <div>Server Id Page</div>;
+import { redirect } from "next/navigation";
+
+import db from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+interface ServerIdPageProps {
+  params: Promise<{ serverId: string }>;
+}
+
+export default async function ServerIdPage({ params }: ServerIdPageProps) {
+  const { serverId } = await params;
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
 }
